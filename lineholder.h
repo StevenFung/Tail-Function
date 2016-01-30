27c1a953933d88401653ed53ecb46f2d15c59797
StevenFung <steven54604@hotmail.com>
2016-01-29 21:53:57 -0500

    Update lineholder.h

--- lineholder.h
+++ lineholder.h
@@ -1,5 +1,16 @@
-#define MAXLINE 2000 
-#define MAXLEN 1000  
-void init_lineholder(int);  
-void insert_line(char *);  
-void print_lines(void); 
+#include <stdio.h>
+#include <string.h>
+
+void init_lineholder(int nlines);
+// read the nlines we want to print
+
+void insert_line(char *line);
+//insert a line to array, but the array should have 10 elements if we didn't giving special command, or the
+// n lines we set because the tail only take the last ten lines of sentences or the last n lines we want, 
+//then we need to free the array if we want to replace the lines to the array
+//then we need to use malloc to find a place to put the memory
+
+
+void print_lines();
+
+//print out the last ten lines or the last n lines we all.
